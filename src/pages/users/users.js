4d7e42ea0c5e0dfabe0c9e797ec44c15f4e5ecf8
Edhar Toomik <edhar.toomik@wsa-047-73b.local>
2021-08-06 17:04:41 +0300

    add all files

--- src/pages/users/users.js
+++ src/pages/users/users.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 function Users() {
-  return <div>Users</div>
+  return <Container className="d-flex justify-content "></Container>
 }
 
 export default Users
